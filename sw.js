8afd838effc13e5491f676b8d68ddd5e545365ac
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2018-07-01 21:03:35 +0100

    fixed file path issues

--- sw.js
+++ sw.js
@@ -3,16 +3,16 @@
 var CACHE_NAME = 'cache-v53';
 
 var urlsToCache = [
-    '/',
+    './',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    '/css/materialize.min.css',
+    './css/materialize.min.css',
     '/css/main.css',
-    '/js/jquery-3.2.1.min.js',
-    '/js/materialize.min.js',
-    '/js/app.js',
-    '/js/promise.js',
-    '/js/fetch.js',
-    '/manifest.json'
+    './js/jquery-3.2.1.min.js',
+    './js/materialize.min.js',
+    './js/app.js',
+    './js/promise.js',
+    './js/fetch.js',
+    './manifest.json'
 ]
 
 //setup install event
