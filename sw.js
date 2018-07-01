f89de1fa47e0448ebbd01e76ab4f24ff42a64bc3
Olufisayo Akinmoju <hfisk000@email.cpcc.edu>
2018-07-01 08:45:03 +0100

    modified serviceworker file

--- sw.js
+++ sw.js
@@ -25,3 +25,49 @@ self.addEventListener('install', function(event){
         })
     )
 });
+
+
+//delete old caches
+self.addEventListener('activate', function(event){
+    console.log('activating service worker')
+    event.waitUntil(
+        caches.keys()
+        .then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if(key != CACHE_NAME){
+                    console.log("removing old cache", key)
+                    return caches.delete(key);
+                }
+            }))
+        })
+    );
+    return self.clients.claim();
+});
+
+//fetch from cache
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response){
+            if(response){
+                return response;
+            }else {
+                return fetch(event.request)
+                .then(function(res){
+                    return caches.open(CACHE_NAME)
+                    .then(function(cache){
+                        cache.put(event.request.url, res.clone() );
+                        return res;
+                    })
+                    .catch(function(err){
+                         console.log(err);
+                 
+
+                    });
+                })
+            };
+        })
+ 
+)
+
+});
